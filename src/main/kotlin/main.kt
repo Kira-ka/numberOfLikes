de50d6239daf0a8d83f3195864ea1eea3c8566ab
Konovalov Kirill <kirkonowalow@gmail.com>
2022-08-28 10:10:16 +0300

    add cycle for

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,18 @@
 fun main() {
-    val likes = 23
+    val likes = 311
     val score = likes % 10
+    var ex = 11
+    for (i in 11..5_000_011 step 100)
+        if (i == likes)
+            ex = i
 
-   if (score == 1){
-       println("человеку")
-   }else{
-       println("людям")
-   }
+
+    if (ex == likes) {
+        println("людям")
+    } else if (score == 1) {
+        println("человеку")
+    } else {
+        println("людям")
+    }
 
 }
